4bef24bfa514e61ceb47c1aa2d876b79a8d4757c
ReshmaR29 <reshmareddy2211@gmail.com>
2025-06-28 18:24:54 +0530

    Feat: add index, genric Book List, Book Detail view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from catalog.models import Book, BorrowedBook, Author, Genre
+from django.views import generic
+
+def index(request):
+    num_books = Book.objects.count()
+    num_instances = BorrowedBook.objects.count()
+    num_instances_available = BorrowedBook.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
