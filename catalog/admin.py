f52f529e3b4b7f3b677d06ed7c06a115f8123d98
ReshmaR29 <reshmareddy2211@gmail.com>
2025-06-28 12:30:36 +0530

    feat: customize admin interface

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
 from .models import Author, Book, BorrowedBook, Genre
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BorrowedBook)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+class BorrowedBookInline(admin.TabularInline):
+    model = BorrowedBook
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines =[BorrowedBookInline]
+
+@admin.register(BorrowedBook)
+class BorrowedBookAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
 admin.site.register(Genre)
+
