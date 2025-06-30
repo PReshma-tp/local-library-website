5e4e17c55f6b69b68ed042d092eee480fed1b1e2
ReshmaR29 <reshmareddy2211@gmail.com>
2025-06-30 15:23:08 +0530

    feat: add visit count to home page

--- catalog/views.py
+++ catalog/views.py
@@ -10,12 +10,16 @@ def index(request):
     num_instances = BorrowedBook.objects.count()
     num_instances_available = BorrowedBook.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits',0)
+    num_visits += 1
+    request.session['num_vists'] = num_visits
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context)
