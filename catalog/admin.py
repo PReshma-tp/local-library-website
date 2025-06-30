18ee012592515e764c1e1b79199c46b3a4f038c8
ReshmaR29 <reshmareddy2211@gmail.com>
2025-06-30 17:29:41 +0530

    Feat: Add user-specific borrowed books view and admin borrower field

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Register your models here.
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
@@ -18,16 +20,30 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
+    list_filter = ('status', 'due_back', 'borrower')
 
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
+
 
 admin.site.register(Genre)
 
